bb725fec2664c427cb9fa304c86044c14e9eac50
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-10-06 11:03:43 +0200

    recipe list draft

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('dummy recipe','decription of dummy recipe','https://www.healthier.qld.gov.au/wp-content/uploads/2015/07/Spagetti-Bolognese-0037-640x420.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {

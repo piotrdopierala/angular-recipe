1622e99e8b0e40c06e3e0f402d9f71da763f590c
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-10-13 19:38:34 +0200

    selecting recipe from list and passing to single recipe component

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes:Recipe[] = [
-    new Recipe('dummy recipe','decription of dummy recipe','https://www.healthier.qld.gov.au/wp-content/uploads/2015/07/Spagetti-Bolognese-0037-640x420.jpg')
+  recipes: Recipe[] = [
+    new Recipe('dummy recipe 1', 'decription of dummy recipe 1', 'https://www.healthier.qld.gov.au/wp-content/uploads/2015/07/Spagetti-Bolognese-0037-640x420.jpg'),
+    new Recipe('dummy recipe 2', 'decription of dummy recipe 2', 'https://www.healthier.qld.gov.au/wp-content/uploads/2015/07/Spagetti-Bolognese-0037-640x420.jpg')
   ];
+
+  @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipeSelected: Recipe) {
+    this.recipeWasSelected.emit(recipeSelected);
+  }
 }
